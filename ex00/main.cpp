80495d119552974e0068be408a89af3e822468d2
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-05-20 14:26:02 +0200

    [UP] main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:05:50 by msharifi          #+#    #+#             */
-/*   Updated: 2023/05/05 16:12:27 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/05/20 14:23:51 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int main()
 	const WrongAnimal* meta = new WrongAnimal();
 	const Animal* j = new Dog();
 	const WrongAnimal* i = new WrongCat();
+	std::cout << meta->getType() << " " << std::endl;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
