d0b999eb6ca4253b958eef248d67b7c62365b3ff
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-05-20 14:38:51 +0200

    [UP] main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:05:50 by msharifi          #+#    #+#             */
-/*   Updated: 2023/05/10 13:17:49 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/05/20 14:35:59 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 #include "Cat.hpp"
 #include "Brain.hpp"
 
-int    main( void )
+int	main( void )
 {
-	Animal    *animals[8];
-	Brain    *brain;
+	Animal	*animals[8];
+	Brain	*brain;
 
 	for (int i = 0; i < 8; i++)
 	{
@@ -27,15 +27,44 @@ int    main( void )
 		else
 			animals[i] = new Cat();
 		std::cout << animals[i]->getType() << std::endl;
+		std::cout << std::endl;
 	}
 	brain = animals[3]->getBrain();
-	brain->ideas[0] = "I'm hungry";
-	brain->ideas[1] = "That's a strange idea I'm having";
-	brain->ideas[2] = "Ball!!!!!";
-	brain->ideas[3] = "Squirrel!!!!!";
+	brain->ideas[0] = "fuck manu";
+	brain->ideas[1] = "Pourquoi ?";
+	brain->ideas[2] = "Je crois pas qu'il y ait de bonne ou de mauvaise siuation...";
+	brain->ideas[3] = "grosse ambiance";
+	std::cout<<"animal 3 = ";
 	std::cout << animals[3]->getBrain()->ideas[0] << std::endl;
-	*(animals[5]) = *(animals[3]);
+
+	*animals[5] = *animals[3];
+	std::cout<<"animal 5 = ";
 	std::cout << animals[5]->getBrain()->ideas[2] << std::endl;
+
+	animals[5]->getBrain()->ideas[2] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	std::cout << animals[3]->getBrain()->ideas[2] << std::endl;
+	std::cout << animals[5]->getBrain()->ideas[2] << std::endl;
+	std::cout << std::endl;
+
 	for (int i = 0; i < 8; i++)
 		delete animals[i];
+	std::cout << std::endl;
+
+	Dog dog1;
+	Dog dog2;
+
+	std::cout<<std::endl;
+	dog1.getBrain()->ideas[12]="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	std::cout << dog1.getBrain()->ideas[12] << std::endl;
+
+	dog2 = dog1;
+	dog1.getBrain()->ideas[12] = "Pourquoi ?";
+	std::cout << dog2.getBrain()->ideas[12] << std::endl;
+	std::cout<<std::endl;
+
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j; // should not create a leak
+	delete i;
+	return (0);
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:05:50 by msharifi          #+#    #+#             */
-/*   Updated: 2023/05/15 12:19:59 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/05/20 14:38:29 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "Cat.hpp"
 #include "Brain.hpp"
 
-int    main( void )
+int	main( void )
 {
 	Animal	*animals[8];
 	Brain	*brain;
@@ -28,15 +28,38 @@ int    main( void )
 		else
 			animals[i] = new Cat();
 		std::cout << animals[i]->getType() << std::endl;
+		std::cout<<std::endl;
 	}
 	brain = animals[3]->getBrain();
-	brain->ideas[0] = "I'm hungry";
-	brain->ideas[1] = "That's a strange idea I'm having";
-	brain->ideas[2] = "Ball!!!!!";
-	brain->ideas[3] = "Squirrel!!!!!";
+	brain->ideas[0] = "Manu n'est pas venu aujourd'hui ce loser";
+	brain->ideas[1] = "Pourquoi ?";
+	brain->ideas[2] = "Je crois pas qu'il y ait de bonne ou de mauvaise siuation...";
+	brain->ideas[3] = "grosse ambiance";
+	std::cout<<"animal 3 = ";
 	std::cout << animals[3]->getBrain()->ideas[0] << std::endl;
-	*(animals[5]) = *(animals[3]);
+
+	*animals[5] = *animals[3];
+	std::cout<<"animal 5 = ";
 	std::cout << animals[5]->getBrain()->ideas[2] << std::endl;
+	std::cout<<std::endl;
+
 	for (int i = 0; i < 8; i++)
 		delete animals[i];
+	std::cout<<std::endl;
+
+	Dog dog1;
+	Dog dog2;
+
+	std::cout<<std::endl<<std::endl;
+	dog1.getBrain()->ideas[12]="qwertyuiop asdfghjkl zxcvbnm";
+	std::cout << dog1.getBrain()->ideas[12] << std::endl;
+
+	dog2 = dog1;
+	std::cout << dog2.getBrain()->ideas[12] << std::endl;
+	std::cout<<std::endl;
+
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j;
+	delete i;
 }
